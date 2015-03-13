52c10c44c42ffb951bc031a1618d190b80960b73
Florian MARGAINE <florian@margaine.com>
2015-03-14 00:51:22 +0100

    Adds the archive function
    Basically, the function defines what the API has to look like and what
    functions I now have to implement.
    
    The archive step is simply done in 2 steps:
    
    1/ Get all the files to archive. This may depend on time. It doesn't if
    the user wants a complete backup.
    2/ Writes the archive. Once all the files are known, then write the
    bytes to a specified file that will make up the archive.

--- src/agni.lisp
+++ src/agni.lisp
@@ -1,1 +1,15 @@
 (in-package #:agni)
+
+
+(defun archive (path-to-archive archive-path &optional (since 0))
+  "Archives all the files and folders"
+  (let ((files (get-files path-to-archive since)))
+    (write-archive archive-path files)))
+
+(defun write-archive (path files)
+  "Writes an archive with all the files"
+  )
+
+(defun get-files (path since)
+  "Gets the files"
+  )
