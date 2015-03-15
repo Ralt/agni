5168d783d9ba4b6029613f7db500bb51fd2362d2
Florian MARGAINE <florian@margaine.com>
2015-03-15 21:42:18 +0100

    Adds the integer-to-octets function
    Copy paste from ironclad to avoid loading the whole library.

--- src/agni.lisp
+++ src/agni.lisp
@@ -13,3 +13,20 @@
        :for element in elements
        :do (setf (elt vector index) element)
        :do (incf index))))
+
+;;;; Taken from ironclad. Not loading the whole library
+;;;; just for this function...
+(defun integer-to-octets (bignum &key (n-bits (integer-length bignum))
+                                (big-endian t))
+  (let* ((n-bytes (ceiling n-bits 8))
+         (octet-vec (make-array n-bytes :element-type '(unsigned-byte 8))))
+    (declare (type (simple-array (unsigned-byte 8) (*)) octet-vec))
+    (if big-endian
+        (loop for i from (1- n-bytes) downto 0
+              for index from 0
+              do (setf (aref octet-vec index) (ldb (byte 8 (* i 8)) bignum))
+              finally (return octet-vec))
+        (loop for i from 0 below n-bytes
+              for byte from 0 by 8
+              do (setf (aref octet-vec i) (ldb (byte 8 byte) bignum))
+              finally (return octet-vec)))))
