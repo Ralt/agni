bc3146dff35aac816ef4638cb62d527d990c5aec
Florian MARGAINE <florian@margaine.com>
2015-03-18 22:35:55 +0100

    Let errors bubble up

--- src/archive.lisp
+++ src/archive.lisp
@@ -3,9 +3,6 @@
 
 (defun write-archive (path files)
   "Writes an archive with all the files"
-  (when (probe-file path)
-    (format t "File ~A already exists.~%" (namestring path))
-    (return-from write-archive))
   (with-open-file (s path
                      :direction :output
                      :element-type '(unsigned-byte 8)
